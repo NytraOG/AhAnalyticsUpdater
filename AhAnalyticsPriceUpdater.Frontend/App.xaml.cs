1ff64965e8b171b3c57f89cdc80e2e942ec07a0a
Christian Nytra <christian.nytra@co-orga.de>
2024-12-13 13:23:12 +0100

    Frontend WPF Client hinzugefügt

---
+++ AhAnalyticsPriceUpdater.Frontend/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AhAnalyticsPriceUpdater.Frontend
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
