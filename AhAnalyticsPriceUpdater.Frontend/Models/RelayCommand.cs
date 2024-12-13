bcda8b06649943fc8f7492f7bf016d3c38b7bac3
Christian Nytra <christian.nytra@co-orga.de>
2024-12-13 14:58:16 +0100

    bining what?

---
+++ AhAnalyticsPriceUpdater.Frontend/Models/RelayCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace AhAnalyticsPriceUpdater.Frontend.Models;
+
+public class RelayCommand(Action execute, Func<bool>? canExecute = null) : ICommand
+{
+    private readonly Action execute = execute ?? throw new ArgumentNullException(nameof(execute));
+
+    public bool CanExecute(object? parameter) => canExecute == null || canExecute();
+
+    public void Execute(object? parameter) => execute();
+
+    public event EventHandler? CanExecuteChanged;
+
+    public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+}
---
+++ AhAnalyticsPriceUpdater.Frontend/Models/UpdateProcessViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace AhAnalyticsPriceUpdater.Frontend.Models;
+
+public class UpdateProcessViewModel : INotifyPropertyChanged
+{
+    public ICommand                           StartProcess { get; }
+    public bool                               InProgress   { get; set; }
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public UpdateProcessViewModel()
+    {
+        StartProcess = new RelayCommand(Start);
+    }
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+            return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    public void Start() => InProgress = true;
+}
